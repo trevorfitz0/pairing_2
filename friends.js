18f259076f69f99711f491f07bb33886d25ba3c6
trevorfitz0 <trevorfitz0@gmail.com>
2022-10-20 16:49:08 -0600

    Add Arrays

--- friends.js
+++ friends.js
@@ -1,5 +1,10 @@
 var arrays = ["bob", "max", "sarah", "daniel", "emma"];
+var locations = ["paris", "london", "nyc", "denver", "hawaii"];
 
 console.log(arrays[2]);
 console.log(arrays[4]);
 console.log(arrays[0]);
+
+locations.push("la");
+
+console.log(locations);
